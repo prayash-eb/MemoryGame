dbab72ab836c6906f2312b36484f0766d51832e6
prayash-eb <prayash.mishra@ebpearls.com>
2025-09-25 16:32:01 +0545

    implemented id for each sticker and handled error in game initialization

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { MemoryGame } from "./game.js";
+import { BoardDimension, StickerType } from "./types/index.js";
+
+const DIMENSION: BoardDimension = {
+    ROW: 5,
+    COLUMN: 4
+}
 
 const Stickers: string[] = ["🍎", "🍌", "🍇", "🍒", "🍉", "🍓", "🍍", "🥝"];
 
-const Game = new MemoryGame("board", 4, 4, Stickers);
-console.log("Game initialized");
-Game.init()
+const StickersInfo: StickerType[] = Stickers.map((sticker, index) => ({
+    id: index + 1,
+    value: sticker,
+}))
+
+// added Error element in html to show error during game initialization
+const errorElement: HTMLElement = document.getElementById("error-message")!
+
+try {
+    const Game = new MemoryGame("board", DIMENSION.ROW, DIMENSION.COLUMN, StickersInfo);
+    Game.init()
+} catch (error: any) {
+    errorElement.textContent = `Error: ${error?.message}`
+}
